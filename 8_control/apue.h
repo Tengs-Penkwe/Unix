470663964bd5868adfdb92969274848eb5f7d9da
Penkwe Cjengh <498296816a@gmail.com>
2021-10-19 14:53:56 +0800

    Study APUE 10.Signal

--- 8_control/apue.h
+++ 8_control/apue.h
@@ -14,3 +14,22 @@
 	break ;}
 #define err_ret(...) { printf(__VA_ARGS__); \
 	return -1 ;}
+
+void pr_exit(int status)
+{
+	if (WIFEXITED(status)) {
+		printf("normal termination, exit status = %d\n",
+				WEXITSTATUS(status));
+	} else if (WIFSIGNALED(status)) {
+		printf("abnormal termination, signal number = %d%s\n",
+				WTERMSIG(status),
+#ifdef WCOREDUMP
+				WCOREDUMP(status) ? " (core file generated)" : "");
+#else
+				"");
+#endif
+	} else if (WIFSTOPPED(status)) { 
+		printf("child stopped, signal number = %d\n",
+				WSTOPSIG(status));
+	}
+}
